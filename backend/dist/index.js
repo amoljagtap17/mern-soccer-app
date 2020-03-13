6736b1b160c006822029344489f2a581984a0337
Amol Jagtap <amoljagtap17@gmail.com>
2020-03-13 14:13:30 +0530

    MERN-SOCCER-APP: Add mongodb connection

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,12 +2,29 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _mongoose = _interopRequireDefault(require("mongoose"));
+
+var _bodyParser = _interopRequireDefault(require("body-parser"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
-var PORT = 3000;
+var PORT = 3000; // mongo connection
+
+_mongoose["default"].Promise = global.Promise;
+
+_mongoose["default"].connect('mongodb://localhost/soccerDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}); // bodyParser setup
+
+
+app.use(_bodyParser["default"].urlencoded({
+  extended: true
+}));
+app.use(_bodyParser["default"].json());
 app.get('/', function (req, res) {
-  return res.send("App is running! on ".concat(PORT));
+  return res.send("App is running on port ".concat(PORT));
 });
 app.listen(PORT, function () {
   return console.log("App is running on localhost : ".concat(PORT));
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import bodyParser from 'body-parser'
 
 const app = express()
 const PORT = 3000
+
+// mongo connection
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/soccerDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// bodyParser setup
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send(`App is running on port ${PORT}`))
 
